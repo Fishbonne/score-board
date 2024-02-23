4b449eb6bade7c0b6b91e09c5016b314ef1bc8f2
Simon <simonkorobov@gmail.com>
2024-02-23 09:20:52 +0300

    Initial commit

--- src/main/java/com/sportradar/core/scoreboard/ScoreBoardApplication.java
+++ src/main/java/com/sportradar/core/scoreboard/ScoreBoardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScoreBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScoreBoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreBoardApplication.class, args);
+    }
 
 }
