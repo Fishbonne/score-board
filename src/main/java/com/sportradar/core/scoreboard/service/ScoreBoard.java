54e30f1b5094ad7b91d6e6763e91421e519e9ac7
Simon <simonkorobov@gmail.com>
2024-02-23 10:26:56 +0300

    feat: score board facade validation

--- src/main/java/com/sportradar/core/scoreboard/service/ScoreBoard.java
+++ src/main/java/com/sportradar/core/scoreboard/service/ScoreBoard.java
@@ -3,16 +3,17 @@ package com.sportradar.core.scoreboard.service;
 
 import com.sportradar.core.scoreboard.domain.MatchDetails;
 import com.sportradar.core.scoreboard.domain.MatchScore;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 public interface ScoreBoard {
 
-    void startNewScore(MatchDetails matchDetails);
+    void startNewScore(@Valid MatchDetails matchDetails);
 
-    void updateScore(MatchScore matchScore);
+    void updateScore(@Valid MatchScore matchScore);
 
-    void finishScore(MatchDetails matchDetails);
+    void finishScore(@Valid MatchDetails matchDetails);
 
     List<MatchScore> listScores();
 
